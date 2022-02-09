5ee02451dec53e667dd9b27a796cb685f0de6f73
Berkay Karakoç <karakocberkay@outlook.com>
2022-02-09 14:08:09 +0300

    add goalsController and routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log("Hello, world");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const { errorHandler } = require("./middlewares/errorMiddleware");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
