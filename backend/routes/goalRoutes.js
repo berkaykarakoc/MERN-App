e24f5617b2a7d51217e5a7a6f34720cd67c8d67a
Berkay Karakoç <karakocberkay@outlook.com>
2022-02-09 18:42:21 +0300

    Rest API for goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controller/goalController");
+const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controllers/goalController");
 
 router.route('/').get(getGoals).post(setGoal);
 router.route('/:id').put(updateGoal).delete(deleteGoal);
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const dotenv = require("dotenv");
-dotenv.config();
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
